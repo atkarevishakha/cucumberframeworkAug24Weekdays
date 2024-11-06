2de9c5e17efe5d249d8eb1133f98e9226feff6e3
kadamsachin10 <kadamsachin629@gmail.com>
2024-11-06 16:24:35 +0530

    changes in Login

--- src/test/java/com/vtiger/pages/LoginPage.java
+++ src/test/java/com/vtiger/pages/LoginPage.java
@@ -2,16 +2,11 @@ package com.vtiger.pages;
 
 import com.aventstack.extentreports.ExtentTest;
 import com.vtiger.utility.PageActions;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 
 public class LoginPage extends PageActions {
 
@@ -59,7 +54,7 @@ public class LoginPage extends PageActions {
 
     public void login(String uid,String pwd)
     {
-
+        //System.out.println("hello");
         SetUserName(uid);
         SetPassword(pwd);
         clickLogin();
